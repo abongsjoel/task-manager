bd970f10a8f5295daa94ca91380f74d1eea14007
TJ <abongsjoel@gmail.com>
2020-10-12 16:53:22 +0100

    Create and basic setup task.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,22 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     //useUnifiedTopology: true
     // useFindAndModify: false
 })
-
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: 'Complete React course        ',
-    
-})
-
