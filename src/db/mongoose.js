8949b99fa6cb68187958c2598f1c437454b9604d
TJ <abongsjoel@gmail.com>
2020-10-13 18:16:51 +0100

    Resouce updating endpoints users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true, 
     //useUnifiedTopology: true
-    // useFindAndModify: false
+    useFindAndModify: false
 })
--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ app.get('/users', async (req, res) => {
 })
 
 app.get('/users/:id', async (req, res) => {
+    
     const _id = req.params.id
 
     try {
@@ -41,6 +42,27 @@ app.get('/users/:id', async (req, res) => {
         res.send(user)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = [ 'name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
