fbe3429e6a2adbb2663aa603d4f67d48e6e7b33f
TJ <abongsjoel@gmail.com>
2020-10-13 22:17:16 +0100

    Securing storing passwords

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -43,7 +44,9 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this
 
-    console.log('Just before saving')
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
 
     next()
 })
