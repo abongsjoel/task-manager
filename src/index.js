cf21eab7b452d00a4baf4bcf71b98efac44169fa
TJ <abongsjoel@gmail.com>
2020-10-13 18:40:50 +0100

    Resource updating endpoints tasks

--- src/index.js
+++ src/index.js
@@ -101,6 +101,27 @@ app.get('/tasks/:id', async (req, res) => {
 
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = [ "description", "completed" ]
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid Updates!' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
