05cb7bc8606f049c848a2f8f7ea2717db4a2ff02
TJ <abongsjoel@gmail.com>
2020-10-14 23:39:22 +0100

    Add support for file upload

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is under maintenamce, please try again later')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -30,14 +28,3 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-// const main = async () => {
-//     // const task = await Task.findById('5f872d9f98a1bd2b848df574')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5f872c48a9ac10369ce2c169')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
