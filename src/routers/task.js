81c418e51a403d564d37654de62bb9f017e5f910
TJ <abongsjoel@gmail.com>
2020-10-14 21:48:06 +0100

    Filtering data

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,9 +17,18 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+//GET /task/completed=true
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
