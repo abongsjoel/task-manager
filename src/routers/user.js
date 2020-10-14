85a032bbfee8a5f4ce4d948cd8a2072406cb3057
TJ <abongsjoel@gmail.com>
2020-10-14 16:54:09 +0100

    Logging out

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,6 +27,28 @@ router.post('/users/login', async (req, res) => {
     }
 }) 
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
