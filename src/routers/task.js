e2843c675ab7815e8b29be784349c49686f796ad
TJ <abongsjoel@gmail.com>
2020-10-14 22:35:20 +0100

    Sort data

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,12 +17,20 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-//GET /task/completed=true
+// GET /task/completed=true
+// GET /task/limit=10&skip=20
+// GET /task/sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try {
         await req.user.populate({
@@ -30,7 +38,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
