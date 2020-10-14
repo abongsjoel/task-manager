42efe336a9bbe12de253e4a34a8e747a49ce4155
TJ <abongsjoel@gmail.com>
2020-10-14 11:41:56 +0100

    Introduce express middlewear

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true, 
-    //useUnifiedTopology: true
+    useUnifiedTopology: true,
     useFindAndModify: false
 })
