96181d355de817ddc7f6972bfb716d04ed99beec
TJ <abongsjoel@gmail.com>
2020-10-15 03:04:41 +0100

    Delete image from user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,4 +102,10 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.status(400).send({ error: error.message })
 })
 
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
 module.exports = router
