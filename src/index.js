5cbd0ec8feea5d739292662eecd3530cbb10f82c
TJ <abongsjoel@gmail.com>
2020-10-15 04:07:40 +0100

    Auto-cropping and image formatting

--- src/index.js
+++ src/index.js
@@ -6,33 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)/)) {
-            return cb(new Error('Please upload a Word document'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -40,8 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
