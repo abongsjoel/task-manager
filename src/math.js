e6c038c70c8ea006f6dc0d21cacbabd51354b9e8
TJ <abongsjoel@gmail.com>
2020-10-16 16:37:17 +0100

    Testing Asynchronous code

--- src/math.js
+++ src/math.js
@@ -1,3 +1,5 @@
+const { set } = require("mongoose")
+
 const calculateTip = (total, tipPercent= .25) => total + (total * tipPercent)
 
 const fahrenheitToCelsius = (temp) => {
@@ -8,9 +10,22 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit, 
+    add
 }
 
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -19,4 +19,23 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 5000)
+// })
+
+test('Should add two numbers', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
